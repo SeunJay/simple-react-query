8a2a9594fafa88d7aae75a5c045c735783cf2ada
ui-cod-tv <uicodetv@googlemail.com>
2021-04-16 11:51:21 +0100

    Initialize project using Create React App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+import axios from "axios";
 
+// This is a typical component with an Asynchronus data fetch 
+// We are using Axios here, but can use standard js fetch command.
 function App() {
+  const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setError(false);
+      setLoading(true);
+
+      try {
+        const response = await axios("http://swapi.dev/api/planets/");
+
+        setData(response.data);
+      } catch (error) {
+        setError(true);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Starwars Planets</h1>
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+        <div>Loading ...</div>
+      ) : (
+        <div>
+           {(JSON.stringify(data) !== '{}') && data.results.map((planet,index) =><p key={index}>{planet.name}</p>)}           
+        </div>
+      )}
     </div>
   );
 }
-
 export default App;
